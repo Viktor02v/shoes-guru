cc4f527d508a615ccbc00ece9d3366b4cc9d18ce
Viktor Petruk <damnnn261@gmail.com>
2024-06-16 22:02:09 +0300

    make topNav and its logic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,91 @@
 <script setup>
+import { ref } from 'vue';
+import { RouterView, RouterLink } from 'vue-router'
+import HeartOutline from 'vue-material-design-icons/HeartOutline.vue';
+import Bell from 'vue-material-design-icons/Bell.vue';
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+
+
+let toggleProfile = ref(false);
+let toggleStatus = ref(false);
 
 </script>
 
 <template>
-	
+	<div id="TopNav"
+		class="fixed  flex items-center justify-between w-[100%] h-[74px] border-b border-b-[#32323D] px-30">
+		<div class="fixed  flex items-center justify-between w-[100%] h-[74px] border-b border-b-[#32323D] ">
+			<div class="flex ml-10 items-center ">
+				<RouterLink to="/">
+					<img width="170" src="/images/orcaShoes logo.png">
+				</RouterLink>
+
+				<div class="w-full">
+					<h1 class="font-semibold text-xl">Orca Shoes</h1>
+					<p class="font-light text-gray-500 text-sm ">Shoesland</p>
+				</div>
+			</div>
+
+			<div class="flex items-center   ">
+				<ul class="flex items-center w-full gap-12">
+					<RouterLink to="/man">
+						<li class="font-semibold text-md cursor-pointer ">FOR MEN</li>
+					</RouterLink>
+					<RouterLink to="/woman">
+						<li class="font-semibold text-md border-l border-l-[#32323D] pl-[35px] cursor-pointer ">FOR WOMEN</li>
+					</RouterLink>
+					<RouterLink to="/children">
+						<li class="font-semibold text-md border-l border-l-[#32323D] pl-[35px] cursor-pointer ">FOR CHILDREN
+						</li>
+					</RouterLink>
+				</ul>
+			</div>
+
+			<div class="flex items-center border rounded-full ">
+				<Magnify class=" pl-3" />
+				<input type="text" placeholder="Search..."
+					class="p-1 bg-transparent outline-none   font-[300] placeholder-[#BEBEC7] text-black w-full max-w-xl">
+			</div>
+
+			<div class="flex items-center mr-10 gap-5">
+				<div>
+					<HeartOutline :size="25" />
+				</div>
+				<div>
+					<Bell :size="25" />
+				</div>
+
+				<button @click="toggleProfile = !toggleProfile" :class="toggleProfile ? 'bg-black' : 'bg-white'" class="rounded-full flex w-[100px] items-center gap-1 p-2 bg">
+					<img width="25" class="rounded-full border "
+						src="https://yt3.ggpht.com/yti/ANjgQV-BsgP7VzRhrVOhoR9oluzj_l7hWizBLiYQpd6u703zSuU=s88-c-k-c0x00ffffff-no-rj"
+						alt="">
+					<RouterLink to="/profile">
+						<button  class=" absolute top-[30px] font-semibold text-[10px]">
+							<div class="text-[#EC7063]" v-if="toggleStatus && toggleProfile">
+								LOGED
+							</div>
+							<div v-else-if="toggleProfile" class="text-[#ABEBC6]">
+								SINGED
+							</div>
+						</button>
+					</RouterLink>
+				</button>
+
+				<span class="absolute z-20 text-center border w-[100px] py-3 px-4 right-10 bg-white top-[60px]" v-if="toggleProfile">
+					<ul class="text-[14px] text-gray font-seminold">
+						<RouterLink to="/profile">
+						<li class="pb-1 border-b border-b-black">Profile</li>
+						</RouterLink>
+						<li v-if="toggleStatus" @click="toggleStatus = !toggleStatus" class="mt-1 cursor-pointer">Sing In</li>
+						<li v-else @click="toggleStatus = !toggleStatus" class="mt-1 cursor-pointer">Log out</li>
+					</ul>
+				</span>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="fixed flex justify-between  w-[100%] mt-[74px] z-10 px-20">
+		<RouterView />
+	</div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,24 @@
 </script>
 
 <template>
-	
+	<div id="Banner" class="banner">
+		Hey there
+	</div>
 </template>
+
+<style>
+.banner {
+	--clr:maroon;
+	width: 100%;
+	height: 70px;
+	background-color: black;
+	display: flex;
+	align-items: center;
+	justify-content: start;
+	font-size: 2.5rem;
+}
+.banner::before {
+	content: "Easy Web Dew";
+	color: white;
+}
+</style>
