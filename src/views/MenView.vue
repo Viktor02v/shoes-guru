11771a11c8eea91197055dd0d0e2998bdf3cd3a5
Viktor Petruk <damnnn261@gmail.com>
2024-07-04 18:36:38 +0300

    check if the <img> is working properly

--- src/views/MenView.vue
+++ src/views/MenView.vue
@@ -12,71 +12,71 @@ let isHoveredForNews = ref(false)
 
 
 	<section id="navigationForMen">
-		<div
-			class="absolute px-[683px] flex center z-30 hover:bg-opacity-100  bg-opacity-20 bg-gradient-to-r from-gray-100 to-gray-300 bg-gray-200">
-			<RouterLink to="/">
-				<button class="flex items-center align-center p-2">
-					<ChevronRight size="20" />
-					<p class="font-light text-center">back</p>
-				</button>
-			</RouterLink>
+		<div class="relative w-[1440px] h-[100%]">
+			<div
+				class="sticky top-0 px-[683px] flex center z-30 hover:bg-opacity-100  bg-opacity-20 bg-gradient-to-r from-gray-100 to-gray-300 bg-gray-200">
+				<RouterLink to="/">
+					<button class="flex items-center align-center p-2">
+						<ChevronRight size="20" />
+						<p class="font-light text-center">back</p>
+					</button>
+				</RouterLink>
+			</div>
 
+			<div class="w-[1440px] overflow-hidden">
+				<div class=" flex flex-col relative  mt-20 w-full mx-auto items-center pb-20 "
+					@mouseenter="isHovered = true">
 
-		</div>
+					<div :class="isHovered ? ' transition duration-600' : 'absolute transition duration-600'"
+						class=" flex items-center w-full justify-between px-20">
+						<div
+							:class="isHovered ? 'translate-x-[0px] transition duration-300 bg-opacity-50' : 'translate-x-[-1370px]  transition duration-300 bg-opacity-0'"
+							class="absolute z-10 translate-y-[-30px] w-[1280px] bg-gradient-to-r from-gray-100 to-black-500 h-[180px] ">
 
-		<div class="w-[1440px] overflow-hidden">
-			<div class=" flex flex-col relative  mt-20 w-full mx-auto items-center pb-20 " @mouseenter="isHovered = true"
-				@mouseleave="isHovered = false">
+						</div>
+						<div
+							:class="isHovered ? 'text-gray-400  translate-x-[610px] translate-y-[60px] transition easy-in duration-300' : 'translate-x-[-300px] translate-y-[-200px] transition easy-out duration-300 text-[20px]'"
+							class="font-light z-20 text-[30px] mb-10">
+							<h2>Men</h2>
+						</div>
 
-				<div :class="isHovered ? ' transition duration-600' : 'absolute transition duration-600'"
-					class=" flex items-center w-full justify-between px-20">
-					<div
-						:class="isHovered ? 'translate-x-[0px] transition duration-300 bg-opacity-50' : 'translate-x-[-1370px]  transition duration-300 bg-opacity-0'"
-						class="absolute z-10 translate-y-[-30px] w-[1280px] bg-gradient-to-r from-gray-100 to-black-500 h-[180px] ">
-
+						<img
+							:class="isHovered ? 'translate-x-[-540px] translate-y-[-30px] transition duration-300' : 'translate-x-[300px] translate-y-[-200px] transition duration-300'"
+							width="200" class=" mb-10 translate-x-[0px] z-30" src="/images/Air Jordan .png">
 					</div>
-					<div
-						:class="isHovered ? 'text-gray-400  translate-x-[610px] translate-y-[60px] transition easy-in duration-300' : 'translate-x-[-300px] translate-y-[-200px] transition easy-out duration-300 text-[20px]'"
-						class="font-light z-20 text-[30px] mb-10">
-						<h2>Men</h2>
-					</div>
 
-					<img
-						:class="isHovered ? 'translate-x-[-539px] translate-y-[-30px] transition duration-300' : 'translate-x-[300px] translate-y-[-200px] transition duration-300'"
-						width="200" class=" mb-10 translate-x-[0px] z-30" src="/images/Air Jordan .png">
+					<ul class="flex font-sans z-20 items-center text-mb  gap-9">
+						<RouterLink to="/events">
+							<li
+								class="font-light hover:font-semibold hover:transition hover:duration-200 text-md cursor-pointer ">
+								New Releases</li>
+						</RouterLink>
+						<RouterLink to="/community">
+							<li
+								class="font-light  hover:font-semibold hover:transition hover:duration-200 text-md border-l border-l-[#32323D] pl-[35px] cursor-pointer ">
+								Community
+							</li>
+						</RouterLink>
+						<RouterLink to="/nike-men">
+							<li
+								class="font-light  hover:font-semibold hover:transition hover:duration-200 text-md border-l border-l-[#32323D] pl-[35px] cursor-pointer ">
+								Nike
+							</li>
+						</RouterLink>
+						<RouterLink to="/jordan-men">
+							<li
+								class="font-light  hover:font-semibold hover:transition hover:duration-200 text-md border-l border-l-[#32323D] pl-[35px] cursor-pointer ">
+								Jordan
+							</li>
+						</RouterLink>
+						<RouterLink to="/help">
+							<li
+								class="font-light  hover:font-semibold hover:transition hover:duration-200 text-md border-l border-l-[#32323D] pl-[35px] cursor-pointer ">
+								Help
+							</li>
+						</RouterLink>
+					</ul>
 				</div>
-
-				<ul class="flex font-sans z-20 items-center text-mb  gap-9">
-					<RouterLink to="/events">
-						<li
-							class="font-light hover:font-semibold hover:transition hover:duration-200 text-md cursor-pointer ">
-							New Releases</li>
-					</RouterLink>
-					<RouterLink to="/community">
-						<li
-							class="font-light  hover:font-semibold hover:transition hover:duration-200 text-md border-l border-l-[#32323D] pl-[35px] cursor-pointer ">
-							Community
-						</li>
-					</RouterLink>
-					<RouterLink to="/nike-men">
-						<li
-							class="font-light  hover:font-semibold hover:transition hover:duration-200 text-md border-l border-l-[#32323D] pl-[35px] cursor-pointer ">
-							Nike
-						</li>
-					</RouterLink>
-					<RouterLink to="/jordan-men">
-						<li
-							class="font-light  hover:font-semibold hover:transition hover:duration-200 text-md border-l border-l-[#32323D] pl-[35px] cursor-pointer ">
-							Jordan
-						</li>
-					</RouterLink>
-					<RouterLink to="/help">
-						<li
-							class="font-light  hover:font-semibold hover:transition hover:duration-200 text-md border-l border-l-[#32323D] pl-[35px] cursor-pointer ">
-							Help
-						</li>
-					</RouterLink>
-				</ul>
 			</div>
 		</div>
 	</section>
@@ -95,7 +95,8 @@ let isHoveredForNews = ref(false)
 						<h2 class="font-sans font-black mb-3 w-[500px] text-center uppercase text-[60px]">
 							Don't waste your blood
 						</h2>
-						<p class="font-light text-center text-[18px]">Run with the violence in your hearth towards the goal.
+						<p class="font-light text-center text-[18px]">Run with the violence burning in your hearth towards the
+							goal.
 							Make it real
 							with the <span class="font-medium text-[18px]"> Nike Violent Red</span> </p>
 						<RouterLink to="/shop">
